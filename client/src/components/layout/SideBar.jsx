1279e9cd1367a98197030405b62f129b0d4bdd8a
Georgi Todorov <georgitodorov6050@gmail.com>
2026-09-08 18:51:13 +0300

    Create all routes with placeholder text inside layout shell

--- client/src/components/layout/SideBar.jsx
+++ client/src/components/layout/SideBar.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Drawer, IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import NavLinks from './NavLinks';
 
 const SideBar = () => {
-  return (
-	<div>SideBar</div>
-  )
-}
+	const [open, setOpen] = useState(false);
 
-export default SideBar
+	const toggleMenu = () => {
+		setOpen((isOpen) => !isOpen);
+	};
+
+	return (
+		<>
+			<IconButton
+				onClick={toggleMenu}
+				size='large'
+				edge='start'
+				color='inherit'
+				aria-label='menu'
+				sx={{ mr: 2 }}>
+				<MenuIcon />
+			</IconButton>
+			<Drawer open={open} onClose={toggleMenu}>
+				<NavLinks toggle={toggleMenu} />
+			</Drawer>
+		</>
+	);
+};
+
+export default SideBar;
